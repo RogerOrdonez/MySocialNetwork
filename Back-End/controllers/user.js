d5bb776450f563a6df840a5fc125ba83bce3a289
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-23 21:41:39 -0600

    Agrego método para crear un usuario

--- Back-End/controllers/user.js
+++ Back-End/controllers/user.js
@@ -17,25 +17,46 @@ function saveUser(request, response) {
         params.email && params.password) {
         user.name = params.name;
         user.surname = params.surname;
-        user.nick = params.nick;
-        user.email = params.email;
+        user.nick = params.nick.toLowerCase();
+        user.email = params.email.toLowerCase();
         user.role = 'ROLE_USER';
         user.image = null;
-        bcrypt.hash(params.password, null, null, (err, hash) => {
-            user.password = hash;
-            user.save((err, userStored) => {
-                if (err) return response.status(500).send({ message: 'Error al guardar el usuario. ' + err });
 
-                if (userStored) {
-                    response.status(200).send({ user: userStored });
-                } else {
-                    response.status(404).send({ message: 'No se ha registrado al usuario' });
-                }
-            });
+        User.findOne({
+            $or: [
+                { email: user.email.toLowerCase() },
+                { nick: user.nick.toLowerCase() }
+            ]
+        }).exec((err, usr) => {
+            if (err) return response.status(500).send({ message: 'Error al consultar en la BD. ' + err });
+
+            if (usr) return response.status(500).send({ message: 'Error al guardar, el usuario o nick ya existen en la BD. ' });
+            else {
+                bcrypt.hash(params.password, null, null, (err, hash) => {
+                    user.password = hash;
+                    user.save((err, userStored) => {
+                        if (err) return response.status(500).send({ message: 'Error al guardar el usuario. ' + err });
+
+                        if (userStored) {
+                            response.status(200).send({ user: userStored });
+                        } else {
+                            response.status(404).send({ message: 'No se ha registrado al usuario' });
+                        }
+                    });
+                });
+            }
         });
+
     } else {
         response.status(200).send({
-            message: 'Faltan datos necesarios'
+            message: 'Faltan datos necesarios',
+            parametros: {
+                name: params.name,
+                surname: params.surname,
+                nick: params.nick,
+                email: params.email,
+                password: params.password
+            }
         });
     }
 
