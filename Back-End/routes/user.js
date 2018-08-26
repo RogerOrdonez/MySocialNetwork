60e313e4d84f59cc82fa98bd791e6de176ce4b24
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-25 20:16:26 -0600

    Agrego ruta para servicio GET que obtiene los datos de un usuarios através del ID

--- Back-End/routes/user.js
+++ Back-End/routes/user.js
@@ -7,6 +7,7 @@ var middleAuth = require('../middlewares/authenticated');
 var api = express.Router();
 
 api.get('/test', middleAuth.ensureAuth, UserController.test);
+api.get('/user/:id', middleAuth.ensureAuth, UserController.getUser);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
