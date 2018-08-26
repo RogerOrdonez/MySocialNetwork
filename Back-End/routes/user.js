83045c761815b41b8f489cc4fc7e7c7a6cb2eddf
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-25 21:27:37 -0600

    Agrego ruta para nuevo método para actualizar información de un usuario.

--- Back-End/routes/user.js
+++ Back-End/routes/user.js
@@ -9,6 +9,7 @@ var api = express.Router();
 api.get('/test', middleAuth.ensureAuth, UserController.test);
 api.get('/user/:id', middleAuth.ensureAuth, UserController.getUser);
 api.get('/users/:page', middleAuth.ensureAuth, UserController.getUsers);
+api.put('/update-user/:id', middleAuth.ensureAuth, UserController.updateUser);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
