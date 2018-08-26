4da5c412dee738a79e63fac5f572e4acfd02d66a
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-25 23:38:24 -0600

    Nuevo método saveFollow para guardar follows entre usuarios

--- Back-End/controllers/follow.js
+++ Back-End/controllers/follow.js
@@ -11,6 +11,23 @@ function test(request, response) {
     response.status(200).send({ message: 'Ruta de pruebas de servidor NodeJS' })
 }
 
+function saveFollow(request, response) {
+    var params = request.body;
+    var follow = new Follow();
+    follow.user = request.user.sub;
+    follow.followed = params.followed;
+    follow.save((err, followStored) => {
+        if (err) {
+            return response.status(500).send({ message: 'Error: No se pudo guardar el follow.' });
+        }
+        if (!followStored) {
+            return response.status(404).send({ message: 'Error: No se guardó el follow.' });
+        }
+        return response.status(200).send({ follow: followStored });
+    });
+}
+
 module.exports = {
-    test
+    test,
+    saveFollow
 }
