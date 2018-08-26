222950848bd54e0ff550a17b604ab585d12a3683
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-25 22:14:36 -0600

    Agrego ruta para método para subir imagenes de usuarios

--- Back-End/routes/user.js
+++ Back-End/routes/user.js
@@ -2,7 +2,9 @@
 
 var express = require('express');
 var UserController = require('../controllers/user');
+var multiParty = require('connect-multiparty');
 var middleAuth = require('../middlewares/authenticated');
+var middleUpload = multiParty({ uploadDir: './uploads/users' });
 
 var api = express.Router();
 
@@ -12,5 +14,6 @@ api.get('/users/:page', middleAuth.ensureAuth, UserController.getUsers);
 api.put('/update-user/:id', middleAuth.ensureAuth, UserController.updateUser);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.post('/upload-image-user/:id', [middleAuth.ensureAuth, middleUpload], UserController.uploadImage);
 
 module.exports = api;
