c44a75ac92e44f757aad117e9e06eb2f721e0c1d
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-26 00:21:45 -0600

    Agrego nuevo método getFollowingUsers para obtener usuarios seguidos

--- Back-End/controllers/follow.js
+++ Back-End/controllers/follow.js
@@ -34,13 +34,39 @@ function deleteFollow(request, response) {
         'user': userId,
         'followed': followId
     }).remove(err => {
-        if (err) response.status(500).send({ message: 'Error: No se pudo eliminar el follow.' });
+        if (err) return response.status(500).send({ message: 'Error: No se pudo eliminar el follow.' });
         response.status(200).send({ message: 'El follow se ha eliminado' });
+    });
+}
+
+function getFollowingUsers(request, response) {
+    var userId = request.user.sub;
+    if (request.params.id && request.params.page) {
+        userId = request.params.id;
+    }
+    var page = 1;
+    if (request.params.page) {
+        page = request.params.page;
+    } else {
+        page = request.params.id;
+    }
+
+    var itemsPerPage = 4;
+    Follow.find({ user: userId }).populate({ path: 'followed' }).paginate(page, itemsPerPage, (err, follows, total) => {
+        if (err) return response.status(500).send({ message: 'Error: No se pudo obtener los follows.' });
+        if (!follows) return response.status(404).send({ message: 'Error: No obtuvo los follows.' });
+        //delete follows.password;
+        response.status(200).send({
+            total,
+            pages: Math.ceil(total / itemsPerPage),
+            following: follows
+        });
     });
 }
 
 module.exports = {
     test,
     saveFollow,
-    deleteFollow
+    deleteFollow,
+    getFollowingUsers
 }
