a9796d844025539814a05e13729e8007a1b48d2f
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-25 22:28:06 -0600

    Agrego ruta para metodo que devuelve imagenes de usuarios

--- Back-End/routes/user.js
+++ Back-End/routes/user.js
@@ -11,6 +11,7 @@ var api = express.Router();
 api.get('/test', middleAuth.ensureAuth, UserController.test);
 api.get('/user/:id', middleAuth.ensureAuth, UserController.getUser);
 api.get('/users/:page', middleAuth.ensureAuth, UserController.getUsers);
+api.get('/get-image-file/:imageFile', middleAuth.ensureAuth, UserController.getImageFile);
 api.put('/update-user/:id', middleAuth.ensureAuth, UserController.updateUser);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
