2f3ed4b20787ab24ad1ddce425dd73fb8bffc981
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-26 21:06:23 -0600

    Agrego ruta para el método getCounters que devuelve estadísticas

--- Back-End/routes/user.js
+++ Back-End/routes/user.js
@@ -11,8 +11,11 @@ var api = express.Router();
 api.get('/test', middleAuth.ensureAuth, UserController.test);
 api.get('/user/:id', middleAuth.ensureAuth, UserController.getUser);
 api.get('/users/:page', middleAuth.ensureAuth, UserController.getUsers);
+api.get('/counters/:id?', middleAuth.ensureAuth, UserController.getCounters);
 api.get('/get-image-file/:imageFile', middleAuth.ensureAuth, UserController.getImageFile);
+
 api.put('/update-user/:id', middleAuth.ensureAuth, UserController.updateUser);
+
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 api.post('/upload-image-user/:id', [middleAuth.ensureAuth, middleUpload], UserController.uploadImage);
