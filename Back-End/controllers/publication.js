7eb4551498e04635f507c4e0f8849ff7ea445342
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-26 21:21:41 -0600

    Nuevo controlador para las publicaciones

---
+++ Back-End/controllers/publication.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var path = require('path');
+var fs = require('fs');
+var moment = require('moment');
+var mongoosePaginate = require('mongoose-pagination');
+
+var Publication = require('../models/publication');
+var User = require('../models/user');
+var Follow = require('../models/follow');
+
+function test(request, response) {
+    response.status(200).send({ message: 'Ruta de pruebas de servidor NodeJS' });
+}
+
+module.exports = {
+    test
+}
