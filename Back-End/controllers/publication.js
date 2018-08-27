0d5c3f47b9d47ad365113d4aa2c78b2a44cb411c
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-26 21:38:35 -0600

    Agrego nuevo método savePublication para guardar publicaciones

--- Back-End/controllers/publication.js
+++ Back-End/controllers/publication.js
@@ -13,6 +13,32 @@ function test(request, response) {
     response.status(200).send({ message: 'Ruta de pruebas de servidor NodeJS' });
 }
 
+function savePublication(request, response) {
+    var params = request.body;
+
+    if (!params.text) {
+        response.status(500).send({ message: 'Debes enviar un texto en la publicación' });
+    }
+
+    var publication = new Publication();
+    publication.text = params.text;
+    publication.file = 'null';
+    publication.user = request.user.sub;
+    publication.created_at = moment().unix();
+
+    publication.save((err, publicationStored) => {
+        if (err) {
+            return response.status(500).send({ message: 'Error al guardar la publicación' });
+        }
+        if (!publicationStored) {
+            return response.status(404).send({ message: 'La publicación no ha sido guardada' });
+        }
+        return response.status(200).send({ publication: publicationStored });
+    });
+
+}
+
 module.exports = {
-    test
+    test,
+    savePublication
 }
