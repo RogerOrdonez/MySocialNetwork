bd2f5de10930a9679afe21cfbd65d85ed0c8c8eb
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-26 21:21:57 -0600

    Agrego ruta de prueba para el controlador de publicaciones

---
+++ Back-End/routes/publication.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var express = require('express');
+var publicationController = require('../controllers/publication');
+var api = express.Router();
+var middleAuth = require('../middlewares/authenticated');
+var multiPart = require('connect-multiparty');
+var middleUpload = multiPart({ uploadDir: './uploads/publications' });
+
+api.get('/test-publication', middleAuth.ensureAuth, publicationController.test);
+
+module.exports = api;
