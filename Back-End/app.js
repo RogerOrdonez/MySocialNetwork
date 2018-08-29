8d886bba678000860be12ed16c4956b0b09550fe
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-28 19:59:17 -0600

    Agrego configuración CORS

--- Back-End/app.js
+++ Back-End/app.js
@@ -15,6 +15,16 @@ var messageRoutes = require('./routes/message');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//CORS
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    response.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
+
 //rutas
 app.use('/api', userRoutes);
 app.use('/api', followRoutes);
