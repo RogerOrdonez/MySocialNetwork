491baebd213f05add1da8e6c678b45c8a4f30514
Róger Ordóñez <roger.ordez@hotmail.com>
2018-08-28 19:32:59 -0600

    Agrego ruta para métod o getReceivedMessages

--- Back-End/routes/message.js
+++ Back-End/routes/message.js
@@ -6,6 +6,7 @@ var api = express.Router();
 var middleAuth = require('../middlewares/authenticated');
 
 api.get('/test-message', middleAuth.ensureAuth, messageController.test);
+api.get('/my-messages/:page?', middleAuth.ensureAuth, messageController.getReceivedMessages);
 
 api.post('/send-message', middleAuth.ensureAuth, messageController.sendMessage);
 
