1f208540fa86afbb845700a65f9507c8371294b6
Róger Ordóñez <roger.ordez@hotmail.com>
2018-09-03 10:31:36 -0600

    Se crea nuevo servicio Upload para subir imagenes

---
+++ Front-End/src/app/services/upload.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+  public url: String;
+  constructor() { 
+    this.url = environment.backendUrl;
+  }
+
+  makeFileRequest(url: string, params: Array<string>, files: Array<File>, token: string) {
+    return new Promise(function(resolve, reject) {
+      const formData = new FormData();
+      const xhr = new XMLHttpRequest();
+      for (let i = 0; i < files.length; i++) {
+        formData.append(name, files[i], files[i].name);
+      }
+      xhr.onreadystatechange = function() {
+        if (xhr.status === 4) {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response);
+          }
+        }
+      };
+
+      xhr.open('POST', url, true);
+      xhr.setRequestHeader('Authorization', token);
+      xhr.send(formData);
+
+    });
+  }
+
+}
