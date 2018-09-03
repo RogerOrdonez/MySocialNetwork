f8c314af966528169ea127b7eeab88da7e466be4
Róger Ordóñez <roger.ordez@hotmail.com>
2018-09-03 17:14:59 -0600

    Agrego navegación del menú para componentes Timeline y Person

--- Front-End/src/app/app.routing.ts
+++ Front-End/src/app/app.routing.ts
@@ -5,12 +5,16 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { PersonComponent } from './components/person/person.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'datos', component: UserComponent },
+  { path: 'timeline', component: TimelineComponent },
+  { path: 'people', component: PersonComponent },
   { path: '**', component: HomeComponent },
 ];
 
