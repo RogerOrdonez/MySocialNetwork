6109f930d28899821f249711ef5474c6a39bafef
Róger Ordóñez <roger.ordez@hotmail.com>
2018-09-02 21:55:59 -0600

    Agrego el componente user a las rutas de la app

--- Front-End/src/app/app.routing.ts
+++ Front-End/src/app/app.routing.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'datos', component: UserComponent },
   { path: '**', component: HomeComponent },
 ];
 
