69a5a45289977bea679a34964ce3043b77520c6b
Róger Ordóñez <roger.ordez@hotmail.com>
2018-09-06 21:30:23 -0600

    Agrego nuevo servicio para realizar follow y unfollow

---
+++ Front-End/src/app/services/follow.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Follow } from '../models/follow.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FollowService {
+  public url: string;
+  constructor(public httpClient: HttpClient, public httpHeaders: HttpHeaders) {
+    this.url = environment.backendUrl;
+  }
+
+  follow(token, follow) {
+    const params = JSON.stringify(follow);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+                                     .set('Authorization', token);
+    return this.httpClient.post(this.url + 'follow', params, {headers: headers});
+  }
+
+  unFollow(token, userId) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+                                     .set('Authorization', token);
+    return this.httpClient.delete(this.url + 'follow/' + userId, {headers: headers});
+  }
+
+}
