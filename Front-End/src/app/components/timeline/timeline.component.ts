26b47eee56b483fab3806e73e1755eac193ef465
Róger Ordóñez <roger.ordez@hotmail.com>
2018-09-07 18:53:44 -0600

    Diseño de la caja para creación de publicaciones

--- Front-End/src/app/components/timeline/timeline.component.ts
+++ Front-End/src/app/components/timeline/timeline.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Publication } from '../../models/publication.model';
+import { UserService } from '../../services/user.service';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-timeline',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.css']
 })
 export class TimelineComponent implements OnInit {
+  public identity;
+  public token;
+  public url;
+  public success;
+  public publication: Publication;
+  public faImage = faImage;
 
-  constructor() { }
+  constructor(private userService: UserService) { 
+    this.identity = this.userService.getIdentity();
+    this.token = this.userService.getToken();
+    this.url = environment.backendUrl;
+    this.publication = new Publication('', '', '', '', '');
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.publication.user = this.identity._id;
+    console.log(this.publication);
   }
 
 }
