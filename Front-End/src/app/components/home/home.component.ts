f9dca3413b59df451319f421a89f35f6285414db
Róger Ordóñez <roger.ordez@hotmail.com>
2018-09-10 21:51:05 -0600

    Ajustes de diseño de la página Home

--- Front-End/src/app/components/home/home.component.ts
+++ Front-End/src/app/components/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faSignInAlt, faSearch, faShareAlt } from '@fortawesome/free-solid-svg-icons';
+
+// sign-in-alt
+// Descubre search
+// Comparte share-alt
 
 @Component({
   selector: 'app-home',
@@ -8,6 +13,9 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   public title: String;
+  public faSignInAlt = faSignInAlt;
+  public faSearch = faSearch;
+  public faShareAlt = faShareAlt;
   constructor(private router: Router) {
     this.title = 'Bienvenido a ngFollow';
     this.router.navigate(['/home']);
