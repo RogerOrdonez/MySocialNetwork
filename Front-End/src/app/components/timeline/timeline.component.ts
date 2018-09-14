4b053a7e281de76d1ea0b30f2ead01bc15bd822d
Róger Ordóñez <roger.ordez@hotmail.com>
2018-09-14 00:19:29 -0600

    Agrego funcionalidad de subir imagen con las publicaciones

--- Front-End/src/app/components/timeline/timeline.component.ts
+++ Front-End/src/app/components/timeline/timeline.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Publication } from '../../models/publication.model';
 import { UserService } from '../../services/user.service';
-import { faImage } from '@fortawesome/free-solid-svg-icons';
+import { faImage, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faTimesCircle as faTimeCircleReg } from '@fortawesome/free-regular-svg-icons';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { retry } from 'rxjs/operators';
 import { PublicationService } from '../../services/publication.service';
+import { UploadService } from '../../services/upload.service';
 
 
 @Component({
@@ -17,9 +19,12 @@ export class TimelineComponent implements OnInit {
   public identity;
   public token;
   public url;
+  public urlImage;
   public success;
   public publication: Publication;
   public faImage = faImage;
+  public faTimesCircle = faTimesCircle;
+  public faTimeCircleReg = faTimeCircleReg;
   public page;
   public pages;
   public publications: Array<Publication>;
@@ -28,8 +33,15 @@ export class TimelineComponent implements OnInit {
   public noMore = false;
   public retry = 0;
   @Input() userId;
+  public filesToUpload: Array<File>;
+  @ViewChild('inputFile') inputFile: ElementRef;
 
-  constructor(private publicationService: PublicationService, private userService: UserService, private router: Router) {
+  constructor(
+      private publicationService: PublicationService,
+      private userService: UserService,
+      private router: Router,
+      private uploadService: UploadService
+  ) {
     this.identity = this.userService.getIdentity();
     this.token = this.userService.getToken();
     this.url = environment.backendUrl;
@@ -48,13 +60,30 @@ export class TimelineComponent implements OnInit {
     this.publicationService.addPublication(this.token, this.publication)
                            .subscribe((response: any) => {
                              if (response.publication) {
-                               this.publication = response.publication;
-                               this.publications.unshift(this.publication);
-                               newPublication.reset();
-                               this.getPublications(1, false, this.userId);
-                               this.success = true;
-                             } else {
-                               this.success = false;
+                                this.publication = response.publication;
+                                this.publications.unshift(this.publication);
+                                // Subir Imagen
+                                if (this.filesToUpload) {
+                                  this.uploadService.makeFileRequest(
+                                      this.url + 'upload-image-pub/' + response.publication._id,
+                                      [],
+                                      this.filesToUpload,
+                                      this.token,
+                                      'image'
+                                    )
+                                    .then((result: any) => {
+                                      this.publication.file = result.image;
+                                    });
+                                  newPublication.reset();
+                                  this.urlImage = null;
+                                  this.filesToUpload = null;
+                                  this.getPublications(1, false, this.userId);
+                                  this.success = true;
+                                } else {
+                                  this.success = false;
+                                }
+                                this.success = true;
+                                this.getPublications(1, false, this.userId);
                              }
                            }
                            ,
@@ -62,6 +91,17 @@ export class TimelineComponent implements OnInit {
                              this.success = false;
                              console.log(<any> error);
                            });
+  }
+
+  fileChangeEvent(inputFile: any) {
+    this.filesToUpload = <Array<File>>inputFile.target.files;
+    if (inputFile.target.files && inputFile.target.files[0]) {
+      const reader = new FileReader();
+      reader.readAsDataURL(inputFile.target.files[0]);
+      reader.onload = (event: any) => {
+        this.urlImage = event.target.result;
+      };
+    }
   }
 
   getPublications(page, adding = false, userId?) {
@@ -110,5 +150,10 @@ export class TimelineComponent implements OnInit {
     // window.scrollTo(0, 0); Ir hasta arriba
   }
 
+  removeImage() {
+    this.urlImage = null;
+    this.filesToUpload = null;
+    this.inputFile.nativeElement.value = '';
+  }
 
 }
