42b6bfc17d0de11cf2ea72f16c52e2079eca9de7
Róger Ordóñez <roger.ordez@hotmail.com>
2018-09-15 11:26:15 -0600

    Ajusto middleware para obtener token de autenticación

--- Back-End/middlewares/authenticated.js
+++ Back-End/middlewares/authenticated.js
@@ -5,13 +5,24 @@ var moment = require('moment');
 var jwtService = require('../services/jwt');
 
 exports.ensureAuth = function(request, response, next) {
-    if (!request.headers.authorization) {
+    var token;
+    if (request.body && request.body.headers && request.body.headers.lazyUpdate) {
+        for (var i = 0; i < request.body.headers.lazyUpdate.length; i++) {
+            if (request.body.headers.lazyUpdate[i].name.toLowerCase() === 'authorization') {
+                token = request.body.headers.lazyUpdate[i].value.replace(/["']+/g, '');
+            }
+        }
+    }
+    if (!request.headers.authorization && !token) {
         return response.status(403).send({
             message: 'La petición no tiene la cabecera de autenticación'
         });
     }
 
-    var token = request.headers.authorization.replace(/["']+/g, '');
+    if (!token) {
+        token = request.headers.authorization.replace(/["']+/g, '');
+    }
+
     try {
         var payload = jwt.decode(token, jwtService.secret);
         if (payload.exp <= moment.unix()) {
