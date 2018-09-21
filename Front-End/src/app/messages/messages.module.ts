41e72e5721eb6663568c842f472c4b8bb295dff5
Róger Ordóñez <roger.ordez@hotmail.com>
2018-09-20 22:22:53 -0600

    Se mueven componentes de mensajes al modulo messages

---
+++ Front-End/src/app/messages/messages.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './components/main/main.component';
+import { AddComponent } from './components/add/add.component';
+import { ReceivedComponent } from './components/received/received.component';
+import { SendedComponent } from './components/sended/sended.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    MainComponent,
+    AddComponent,
+    ReceivedComponent,
+    SendedComponent
+  ]
+})
+export class MessagesModule { }
